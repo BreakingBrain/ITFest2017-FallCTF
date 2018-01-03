5ac0d52dc8884145df9106d4fd8ae5b13678d830
tiso <tiso@breakingbrain.org>
2018-01-03 13:22:29 +0300

    Added crypto100 task and write-up

---
+++ crypto/cannotjust/source/crypt.py
@@ -0,0 +1,14 @@
+
+key = "lol"
+
+with open('crypted.jpg', 'wb') as fout:
+    with open('boromir1.jpg', 'rb') as fin:
+        idx = 0
+
+        byte = fin.read(1)
+        while byte:
+            v = int.from_bytes(byte, byteorder='big')
+            b = v ^ ord(key[idx])
+            idx = (idx + 1) % 3
+            fout.write(bytes([b]))
+            byte = fin.read(1)
